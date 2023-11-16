58ea0509afc36a146bfd7d0594cdab15fe984645
文莉李 <wenlili@wenlilidembp.mynetworksettings.com>
2023-11-15 17:47:23 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentsRoutes from "./assignments/routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+Hello(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentsRoutes(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
