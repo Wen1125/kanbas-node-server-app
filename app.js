d63b188425e666c82e40a302b94dc181ddfd2d20
文莉李 <wenlili@wenlilidembp.mynetworksettings.com>
2023-12-02 01:44:27 -0800

    a6

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import cors from "cors";
+import UserRoutes from "./users/routes.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentsRoutes from "./assignments/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
 
-
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 Hello(app);
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentsRoutes(app);
