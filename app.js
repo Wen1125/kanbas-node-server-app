2da9adddc131935f50c8bf077d4826458c1b15b6
文莉李 <wenlili@wenlilideMacBook-Pro.local>
2023-12-03 18:39:58 -0800

    complete a6

--- app.js
+++ app.js
@@ -9,11 +9,34 @@ import ModuleRoutes from "./modules/routes.js";
 import AssignmentsRoutes from "./assignments/routes.js";
 import "dotenv/config";
 import mongoose from "mongoose";
+import session from "express-session";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+// mongodb+srv://Wenli:<password>@atlascluster.tlh0vks.mongodb.net/?retryWrites=true&w=majority
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    })
+);   
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+sessionOptions.proxy = true;
+sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    };
+}
+app.use(
+    session(sessionOptions)
+);
 app.use(express.json());
 Hello(app);
 UserRoutes(app);
